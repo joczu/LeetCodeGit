e17569bad03695d5a8d98f41ad6b0412a4cbce4d
joczu <531902449@qq.com>
2020-04-22 13:54:44 +0800

    多种解法/经典题目

---
+++ 53-最大子序和.h
@@ -0,0 +1,145 @@
+//
+// Created by Joc zu on 2020/4/22.
+//
+
+#ifndef LEETCODEGIT_53_最大子序和_H
+#define LEETCODEGIT_53_最大子序和_H
+
+#endif //LEETCODEGIT_53_最大子序和_H
+
+#include <vector>
+using namespace std;
+
+// 暴力求解
+class Solution
+{
+public:
+    int maxSubArray(vector<int> &nums)
+    {
+        //类似寻找最大最小值的题目，初始值一定要定义成理论上的最小最大值
+        int max = INT_MIN;
+        int numsSize = int(nums.size());
+        for (int i = 0; i < numsSize; i++)
+        {
+            int sum = 0;
+            for (int j = i; j < numsSize; j++)
+            {
+                sum += nums[j];
+                if (sum > max)
+                {
+                    max = sum;
+                }
+            }
+        }
+
+        return max;
+    }
+};
+
+/*
+ * 动态规划法
+class Solution
+{
+public:
+    int maxSubArray(vector<int> &nums)
+    {
+        //类似寻找最大最小值的题目，初始值一定要定义成理论上的最小最大值
+        int result = INT_MIN;
+        int numsSize = int(nums.size());
+        //dp[i]表示nums中以nums[i]结尾的最大子序和
+        vector<int> dp(numsSize);
+        dp[0] = nums[0];
+        result = dp[0];
+        for (int i = 1; i < numsSize; i++)
+        {
+            dp[i] = max(dp[i - 1] + nums[i], nums[i]);
+            result = max(result, dp[i]);
+        }
+
+        return result;
+    }
+};
+链接：https://leetcode-cn.com/problems/maximum-subarray/solution/zui-da-zi-xu-he-cshi-xian-si-chong-jie-fa-bao-li-f/
+ */
+
+/*
+ * 贪心法
+class Solution
+{
+public:
+    int maxSubArray(vector<int> &nums)
+    {
+        //类似寻找最大最小值的题目，初始值一定要定义成理论上的最小最大值
+        int result = INT_MIN;
+        int numsSize = int(nums.size());
+        int sum = 0;
+        for (int i = 0; i < numsSize; i++)
+        {
+            sum += nums[i];
+            result = max(result, sum);
+            //如果sum < 0，重新开始找子序串
+            if (sum < 0)
+            {
+                sum = 0;
+            }
+        }
+
+        return result;
+    }
+};
+链接：https://leetcode-cn.com/problems/maximum-subarray/solution/zui-da-zi-xu-he-cshi-xian-si-chong-jie-fa-bao-li-f/
+ */
+
+/*
+ * 分治法
+class Solution
+{
+public:
+    int maxSubArray(vector<int> &nums)
+    {
+        //类似寻找最大最小值的题目，初始值一定要定义成理论上的最小最大值
+        int result = INT_MIN;
+        int numsSize = int(nums.size());
+        result = maxSubArrayHelper(nums, 0, numsSize - 1);
+        return result;
+    }
+
+    int maxSubArrayHelper(vector<int> &nums, int left, int right)
+    {
+        if (left == right)
+        {
+            return nums[left];
+        }
+        int mid = (left + right) / 2;
+        int leftSum = maxSubArrayHelper(nums, left, mid);
+        //注意这里应是mid + 1，否则left + 1 = right时，会无线循环
+        int rightSum = maxSubArrayHelper(nums, mid + 1, right);
+        int midSum = findMaxCrossingSubarray(nums, left, mid, right);
+        int result = max(leftSum, rightSum);
+        result = max(result, midSum);
+        return result;
+    }
+
+    int findMaxCrossingSubarray(vector<int> &nums, int left, int mid, int right)
+    {
+        int leftSum = INT_MIN;
+        int sum = 0;
+        for (int i = mid; i >= left; i--)
+        {
+            sum += nums[i];
+            leftSum = max(leftSum, sum);
+        }
+
+        int rightSum = INT_MIN;
+        sum = 0;
+        //注意这里i = mid + 1，避免重复用到nums[i]
+        for (int i = mid + 1; i <= right; i++)
+        {
+            sum += nums[i];
+            rightSum = max(rightSum, sum);
+        }
+        return (leftSum + rightSum);
+    }
+};
+链接：https://leetcode-cn.com/problems/maximum-subarray/solution/zui-da-zi-xu-he-cshi-xian-si-chong-jie-fa-bao-li-f/
+ */
